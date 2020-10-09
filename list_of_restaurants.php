e0aa6f40450cf541d9e288550970886a796e4ca7
zabed.emdad <48093696+Zabedz@users.noreply.github.com>
2020-10-09 17:50:50 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -25,26 +25,26 @@
             $serverName = "tcp:lab2tp050051.database.windows.net,1433";
             $conn = sqlsrv_connect($serverName, $connectionInfo);
             if (!$conn)
-            {
-              die("Error connection: ".sqlsrv_errors());
-            }
-            $tsql= "SELECT * FROM [dbo].[restaurant]";
-            $getResults= sqlsrv_query($conn, $tsql);
-            if ($getResults == FALSE)
-            {
-              die(sqlsrv_errors());
-            }
-            while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
-            {
-              echo "<tr>";
-              echo "<td>". $row['restaurant_id'] . "</td>";
-              echo "<td>". $row['restaurant_name'] ."</td>";
-              echo "<td>". $row['restaurant_address'] . "</td>";
-              echo "<td>". $row['restaurant_phone'] . "</td>";
-              echo "</tr>";
-            }
-            sqlsrv_free_stmt($getResults);
-        ?>
+              {
+                die("Error connection: ".sqlsrv_errors());
+              }
+              $tsql= "SELECT * FROM [dbo].[restaurant]";
+              $getResults= sqlsrv_query($conn, $tsql);
+              if ($getResults == FALSE)
+              {
+                die(sqlsrv_errors());
+              }
+              while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+              {
+                echo "<tr>";
+                echo "<td>". $row['restaurant_id'] . "</td>";
+                echo "<td>". $row['restaurant_name'] ."</td>";
+                echo "<td>". $row['restaurant_address'] . "</td>";
+                echo "<td>". $row['restaurant_phone'] . "</td>";
+                echo "</tr>";
+              }
+              sqlsrv_free_stmt($getResults);
+            ?>
       </table>
     </center>
   </body>
